ae4e90df735031c49eea22c497752bb7035a8ac6
@thien <thien@users.noreply.github.com>
2019-06-18 22:11:04 +0100

    add support for scraping hansards

--- datasets/initiate.sh
+++ datasets/initiate.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo "what"
 mkdir -p machine_translation
 mkdir -p newspapers
+
+
+#
+#   GLOBAL VOICES DATASET
+#
 
 # download global_voices dataset
 if [ -e machine_translation/global_voices/dataset.tgz ]
@@ -16,18 +20,20 @@ else
 fi
 
 # extract dataset.
-if [-e machine_translation/global_voices/training/globalvoices.fr-en.en]
+if [ -e machine_translation/global_voices/training/globalvoices.fr-en.en ]
 then
-    echo "dataset is already extracted."
+    echo "global voices dataset is already extracted."
 else
-    tar -xvf machine_translation/global_voices/dataset.tgz
+    cd machine_translation/global_voices
+    tar -xvf dataset.tgz
+    cd ../..
     echo "extracted dataset."
 fi
 
 # need to worry about cleaning the dataset because the english variant has a bunch of rubbish on it.
-if [-e machine_translation/global_voices/globalvoices.en]
+if [ -e machine_translation/global_voices/globalvoices.en ]
 then
-    echo "translation corpus is already extracted."
+    echo "global voices translation corpus is already extracted."
 else
     cd machine_translation/global_voices
     python3 clean.py -folder_path training -source fr -target en
@@ -35,3 +41,42 @@ else
     echo "extracted corpus."
 fi
 
+
+#
+#   HANSARDS
+#
+
+# check if we own the hansards dataset already.
+mkdir -p machine_translation/hansards
+if [ -e machine_translation/hansards/hansard.36 ]
+then 
+    echo "hansards already extracted."
+else
+    echo -n "downloading hansard corpus.. (around 83mb)"
+    cd machine_translation/hansards
+    wget -O house_training.tar -q http://www.isi.edu/natural-language/download/hansard/hansard.36.r2001-1a.house.debates.training.tar
+    wget -O house_testing.tar -q http://www.isi.edu/natural-language/download/hansard/hansard.36.r2001-1a.house.debates.testing.tar
+    wget -O senate_training.tar -q http://www.isi.edu/natural-language/download/hansard/hansard.36.r2001-1a.senate.debates.training.tar
+    wget -O senate_testing.tar -q http://www.isi.edu/natural-language/download/hansard/hansard.36.r2001-1a.senate.debates.testing.tar
+    echo " done."
+    echo -n "extracting tarfiles.."
+    tar xvf house_training.tar 
+    tar xvf house_testing.tar
+    tar xvf senate_training.tar
+    tar xvf senate_testing.tar
+    gzip -d -r hansard.36
+    echo " done."
+    cd ../..
+fi
+
+# deal with extracting the whole corpus.
+if [ -e machine_translation/hansards/hansards.en ]
+then 
+    echo "hansards already scraped."
+else
+    echo -n "scraping hansards.."
+    cd machine_translation/hansards
+    python3 scrape.py
+    echo " done."
+    cd ../..
+fi
---
+++ datasets/machine_translation/hansards/scrape.py
@@ -0,0 +1,48 @@
+import os
+
+"""
+quick script to go through the hansards files.
+"""
+
+# you'll want to have a recursive browser.
+def snoop(path):
+    ents = []
+    entries = os.listdir(path)
+    if len(entries) > 5:
+        ents.append(path)
+    for ent in entries:
+        direct_path = os.path.join(path,ent)
+        if os.path.isdir(direct_path):
+            ents += snoop(direct_path)
+    return ents
+
+# get the folders that contain the relevant files.
+paths = snoop("hansard.36")
+
+# this dictionary is structured as such `en` is the key, and `fr` is the value.
+corpus = {}
+
+# since this is a french-english corpus we can hardcode the values no problem.
+enc = "ISO-8859-1"
+for folder in paths:
+    entries = set([filename[:-2] for filename in os.listdir(folder)])
+    for entry in entries:
+        entry = os.path.join(folder, entry)
+        fr_file, en_file = entry + ".f", entry + ".e"
+        # print(fr_file)
+        with open(fr_file, encoding=enc) as f, open(en_file, encoding=enc) as r:
+            for f_txt, e_txt in zip(f,r):
+                f_txt, e_txt = f_txt.strip(), e_txt.strip()
+                """note: "Neither the sentence splitting nor the alignments are perfect. In particular, watch out for pairs that differ considerably in length. You may want to filter these out before you do any statistical training." -> https://www.isi.edu/natural-language/download/hansard/"""
+                if len(f_txt) < 1 or len(e_txt) < 1:
+                    continue
+                corpus[e_txt] = f_txt
+
+# now we need to save these files.
+filename = "hansards"
+filepath = filename + "."
+p = 'w' # <-- permissions.
+with open(filepath+"en", p) as e, open(filepath+"fr", p) as f:
+    for e_txt in corpus:
+        e.write(e_txt+"\n")
+        f.write(corpus[e_txt]+"\n")

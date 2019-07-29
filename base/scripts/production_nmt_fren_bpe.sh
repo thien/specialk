667363084e53371f03f86bcf9c3f8a5328bd7b01
@thien <thien@users.noreply.github.com>
2019-07-29 17:13:21 +0100

    move dataset creation elsewhere

--- base/scripts/production_nmt_fren_bpe.sh
+++ base/scripts/production_nmt_fren_bpe.sh
@@ -10,7 +10,7 @@ TEST_EN=$FILEPATH"corpus_enfr.test.en"
 TEST_FR=$FILEPATH"corpus_enfr.test.fr"
 
 FORMAT="bpe"
-MAXLEN="500"
+MAXLEN="200"
 PTF=".pt"
 B="_base"
 m="models/"
@@ -20,6 +20,18 @@ FR_ENFR="nmt_enfr_full"$EXT
 FR_FREN="nmt_fren_full"$EXT
 SAVENAME_FR_ENFR=$m$FR_ENFR
 SAVENAME_FR_FREN=$m$FR_FREN
+
+if [ -e ../datasets/machine_translation/corpus_enfr.test.en ]
+then
+    if [ -e $SAVENAME_FR_FREN$PTF ]
+    then
+        echo "xl corpus already created."
+    else
+        # python3 preprocess.py -train_src $TRAIN_EN -train_tgt $TRAIN_FR -valid_src $VALID_EN -valid_tgt $VALID_FR -format $FORMAT -max_len $MAXLEN -save_name $SAVENAME_FR_ENFR  -vocab_size $VOCAB_SIZE
+
+        python3 preprocess.py -train_src $TRAIN_FR -train_tgt $TRAIN_EN -valid_src $VALID_FR -valid_tgt $VALID_EN -format $FORMAT -max_len $MAXLEN -save_name $SAVENAME_FR_FREN  -vocab_size $VOCAB_SIZE
+    fi
+fi
 
 
 # Train nmt models.

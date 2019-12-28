a67abba7975b90f7d269767a379c7fbecf3977a5
@thien <thien@users.noreply.github.com>
2019-12-28 20:57:18 +0000

    fix gigatext entries

--- datasets/machine_translation/10-9/get.sh
+++ datasets/machine_translation/10-9/get.sh
@@ -1,9 +1,22 @@
-wget http://www.statmt.org/wmt10/training-giga-fren.tar
-tar -xvf training-giga-fren.tar
-gunzip giga-fren.release2.fixed.en.gz 
-gunzip giga-fren.release2.fixed.fr.gz 
-rm giga-fren.release2.fixed.fr.gz 
-rm giga-fren.release2.fixed.en.gz
-python3 filter_giga.py
-rm giga-fren.release2.fixed.en
-rm giga-fren.release2.fixed.fr
+# download the tar and extract it.
+if [ -e ./training-giga-fren.tar ]
+then
+    echo "already downloaded gigatext."
+else
+    wget http://www.statmt.org/wmt10/training-giga-fren.tar
+fi
+
+if [ -e ./gigafren.en ]
+then 
+    echo "already extracted gigatext."
+else
+    tar -xvf ./training-giga-fren.tar
+    # extract each language
+    gunzip ./giga-fren.release2.fixed.en.gz 
+    gunzip ./giga-fren.release2.fixed.fr.gz 
+    # filter gigatext.
+    python3 ./filter_giga.py
+    # delete ogs
+    rm ./giga-fren.release2.fixed.en
+    rm ./giga-fren.release2.fixed.fr
+fi

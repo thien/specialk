394d5f44b66b95566a49d6dfd70a2efc2a6668dc
@thien <thien@users.noreply.github.com>
2020-01-06 15:59:08 +0000

    remove redundant code

--- base/scripts/sync.sh
+++
@@ -1,4 +0,0 @@
-# to be called from the azure instance.
-cd ..
-rsync -av models/ t@casper.thien.io:/media/data/azure_dumps
-python3 core/telegram.py -m "Finished transferring files to local machine. You need to turn off the instance!"

18fcf144913216c0c963e64af7499f092625ba6c
@thien <thien@users.noreply.github.com>
2024-08-08 14:46:17 -0400

    remove redundant files

--- specialk/models/recurrent/__init__.py
+++ specialk/models/recurrent/__init__.py
@@ -3,12 +3,10 @@ from specialk.models.recurrent.Beam import Beam
 
 from specialk.models.recurrent.Optim import Optim
 from specialk.models.recurrent.Translator import Translator as Translator
-from specialk.models.recurrent.Translator_style import Translator as Translator_style
 
 __all__ = [
     Models,
     Optim,
     Translator,
-    Translator_style,
     Beam,
 ]

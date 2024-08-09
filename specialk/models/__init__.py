c47acf9bd6b49c4fa1a7c6c6417edcaca6b91880
@thien <thien@users.noreply.github.com>
2024-08-09 14:37:27 -0400

    relative import

--- specialk/models/__init__.py
+++ specialk/models/__init__.py
@@ -1,11 +1,3 @@
-# import transformer
-# import recurrent
-# from TransformerModel import TransformerModel
-# # from RecurrentModel import RecurrentModel
-
-# __all__ = [transformer, recurrent, TransformerModel]
-
-
 import specialk.models.recurrent as recurrent
 import specialk.models.transformer.legacy as transformer
 
--- specialk/models/recurrent/__init__.py
+++ specialk/models/recurrent/__init__.py
@@ -1,7 +1,8 @@
-import specialk.models.recurrent.models as models
 from specialk.models.recurrent.attention import Attention
 from specialk.models.recurrent.beam import Beam
 from specialk.models.recurrent.optim import Optim
 from specialk.models.recurrent.translator import Translator
+
+from . import models as models
 
 __all__ = [models, Optim, Translator, Beam, Attention]

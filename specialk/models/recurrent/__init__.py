393eeb2c8959b441a652ff7221e34fe28ef342fe
@thien <thien@users.noreply.github.com>
2024-09-25 13:24:53 +0100

    remove redundant implementations for Beam; Optim; Translator

--- specialk/models/recurrent/__init__.py
+++ specialk/models/recurrent/__init__.py
@@ -1,7 +1,4 @@
+import specialk.models.recurrent.models as Models
 from specialk.models.recurrent.attention import Attention
-import specialk.models.recurrent.models as Models 
-from specialk.models.recurrent.beam import Beam
-from specialk.models.recurrent.optim import Optim
-from specialk.models.recurrent.translator import Translator
 
-__all__ = [Models, Optim, Translator, Beam, Attention]
+__all__ = [Models, Attention]
